38c633485255ea9065b5f4fdfe9aff7627f0cbbf
Y.S. <166080013+eliz-amst@users.noreply.github.com>
2024-10-09 14:03:52 -0500

    Update my_ls.h
    updated header file

--- my_ls.h
+++ my_ls.h
@@ -10,7 +10,12 @@
 
 #define EXIT_FAILURE 1
 #define MAX_ENTRIES 1000
-int list_dir(const char *dir, int op_a, int op_t);
+
+int my_strcmp(const char *a, const char *b);
+void copy_dirent(struct dirent *dest, const struct dirent *src);
+void sort_name(struct dirent *entries[], int count);
+void sort_time(struct dirent *entries[], struct timespec *times, int count);
+int list_dir(const char *dir_name, int op_a, int op_t);
 int main(int argc, char **argv);
 
 #endif
